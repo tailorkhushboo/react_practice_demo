59392da4b8a9deeb821825d936f2c18b64d710bc
JAY TAILOR <Tailorj64@gmail.com>
2023-07-05 22:25:08 +0530

    Update the name of home link

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
-                 My Home
+                  My Awesome Home
                 </a>
               </li>
               <li className="nav-item">
@@ -92,7 +92,6 @@ function App() {
           </div>
         </div>
       </nav>
-      
     </>
   );
 }
